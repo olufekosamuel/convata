a3ea4da8b9799d54db860d0bd61eb4dba03ee172
Sam <mololuwasamuel12@gmail.com>
2020-05-19 17:03:40 +0100

    update: still in progress

--- src/App.js
+++ src/App.js
@@ -4,21 +4,48 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <div className="text-center header">
+          <h1 className="exchange">Always get the real exchange rate</h1>
+          <p className="headerbottomtext">Real time life update on currencies.</p>
+        </div>
+
+        <div className="currencyconverter">
+          <div className="row">
+            <div className="col-md-6">
+                <p className="currencyform">From</p>
+                <select name="account"  className="form-control currencySelect round">
+                      <option>
+                      GBP British Pound
+                        <div className="row">
+                          <div className="col-md-4"></div>
+                          <div className="col-md-4"></div>
+                          <div className="col-md-4">Logo</div>
+                        </div>
+                        
+                          </option>
+                      <option> DOL United state </option>
+                      <option> NGR Niger state </option>
+                </select>
+                <input type="text" value="100" className="form-control currencyInput" placeholder=""/>
+                <div className="row">
+                  <div className="col-md-3"><p className="currencyform">Current rate</p><p className="currentrate">1.2847</p></div>
+                  <div className="col-md-9"><p className="currencyform">Todays change</p><p className="badrate">0.0016 (0.12%)</p></div>
+                </div>
+            </div>
+            <div className="col-md-6">
+                <p className="currencyform">To</p>
+                  <select name="account"  className="form-control currencySelect round">
+                        <option> GBP British Pound </option>
+                  </select>
+                <input type="text" value="100" className="form-control currencyInput" placeholder=""/>
+                <div className="row">
+                  
+                </div>
+            </div>
+          </div>
+        </div>
+        
     </div>
   );
 }
